c08305efcc37ec89e0c8bca5cc24f137436929c5
Golovadvauxa <parenek.serega@gmail.com>
2021-12-20 15:50:44 +0300

    initial

---
+++ 01_inhabitants.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Вывести на консоль жителей комнат (модули room_1 и room_2)
+# Формат: В комнате room_1 живут: ...
+
+import room_1 as r1, room_2 as r2
+
+print('В комнате room_1 живут:', r1.folks,
+      '\nВ комнате room_2 живут:', r2.folks)
