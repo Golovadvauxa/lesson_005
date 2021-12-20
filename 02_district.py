89e7798cd36f6b7af43d495f4bdef8c4215d55b1
Golovadvauxa <parenek.serega@gmail.com>
2021-12-20 16:18:55 +0300

    exercise '02_district' done

--- 02_district.py
+++ 02_district.py
@@ -6,7 +6,17 @@
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
 # TODO здесь ваш код
+from pprint import pprint
+from district.central_street.house1 import room1 as central1_r1, room2 as central1_r2
+from district.central_street.house2 import room1 as central2_r1, room2 as central2_r2
 
+from district.soviet_street.house1 import room1 as soviet1_r1, room2 as soviet1_r2
+from district.soviet_street.house2 import room1 as soviet2_r1, room2 as soviet2_r2
 
+population = (central1_r1.folks, central1_r2.folks, central2_r1.folks, central2_r2.folks,
+              soviet1_r1.folks, soviet1_r2.folks, soviet2_r1.folks, soviet2_r2.folks)
 
-
+persons = 'На районе живут: '
+for i in population:
+    persons += ','.join(i)+','
+pprint(persons)
